18536e1d4d7c7e7c26d8ea4ee2cea9f35211f503
baileyadler <526BAdler@frhsd.com>
2024-12-13 09:23:28 -0500

    added  more js

--- script.js
+++ script.js
@@ -66,13 +66,27 @@ function genFullName() {
   const firstName = document.getElementById("firstName").value.trim();
   const lastName = document.getElementById("lastName").value.trim();
   const roadType = document.getElementById("roadType").value;
-  const favColor = document.getElementById("favColor").value.trim();
-  const favAnimal = document.getElementById("favAnimal").value.trim();
-}
+  const favoriteColor = document.getElementById("favoriteColor").value.trim();
+  const favoriteAnimal = document.getElementById("favoriteAnimal").value.trim();
 
-//generate each part of the name using helper functions
-const prefix = genPrefix(firstName);
-const newFirstName = genFirstName(firstName);
-const genMiddleName = genMiddleName(roadType, favColor);
-const newLastName = genLastName(lastName);
-const suffix = genSuffix(favAnimal);
+  //generate each part of the name using helper functions
+  const prefix = genPrefix(firstName);
+  const newFirstName = genFirstName(firstName);
+  const genMiddleName = genMiddleName(roadType, favColor);
+  const newLastName = genLastName(lastName);
+  const suffix = genSuffix(favAnimal);
+  // functions to capital word
+  const capitalizedPrefix = tocapitalize(prefix);
+  const capitalizedFirstName = tocapitalize(newFirstName);
+  const capitalizedMiddleName = tocapitalize(MiddleName);
+  const capitalizedLastName = tocapitalize(newLastName);
+
+  //combine all parts to create full name
+  const fullName = `${capitalizePrefix} ${capitalizedFirstName} ${capitalizedMiddleName} ${capitalizedLastName}`;
+
+  document.getElementById("result").textContent = fullName;
+}
+//capitalizer function
+function tocapitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
